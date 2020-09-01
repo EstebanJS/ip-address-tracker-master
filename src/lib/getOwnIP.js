a97337461407c05a5f5e584767d2123e92f7d06c
Esteban Jimenez <yeisson310@hotmail.com>
2020-08-31 20:24:35 -0500

    Esteban Jimenez fixed isue ip in vercel

--- src/lib/getOwnIP.js
+++ src/lib/getOwnIP.js
@@ -1,7 +1,9 @@
 const getOwnIp = async () => {
-    const rest = await fetch('http://api.ipify.org/?format=json');
-    const result = await rest.json()
-    return result.ip
+    // Not for vercel
+    // const rest = await fetch('http://api.ipify.org/?format=json');
+    // const result = await rest.json()
+    // return result.ip
+    return 'www.google.com'
 }
 
 export default getOwnIp

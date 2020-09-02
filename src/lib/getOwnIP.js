fbaa5b6c54ffc0dcc929515a5686dedccd11f044
Esteban Jimenez <yeisson310@hotmail.com>
2020-09-02 15:38:50 -0500

    Esteban Jimenez test of the other request

--- src/lib/getOwnIP.js
+++ src/lib/getOwnIP.js
@@ -1,9 +1,9 @@
 const getOwnIp = async () => {
     // Not for vercel
-    // const rest = await fetch('http://api.ipify.org/?format=json');
-    // const result = await rest.json()
-    // return result.ip
-    return 'www.google.com'
+    const rest = await fetch('https://cors-anywhere.herokuapp.com/http://api.ipify.org/?format=json');
+    const result = await rest.json()
+    return result.ip
+    // return 'www.google.com'
 }
 
 export default getOwnIp
